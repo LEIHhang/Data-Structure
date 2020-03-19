f969fc09efa6c655b6052a3fd86087896ea2df99
LEIHhang <48359244+LEIHhang@users.noreply.github.com>
2020-03-19 20:38:43 +0800

    updata

--- AVL_Tree/AVLTree.hpp
+++ AVL_Tree/AVLTree.hpp
@@ -9,7 +9,7 @@ struct AVLTreeNode
 	pair<K, V> _kv;
 	int _bf;
 
-	AVLTreeNode(pair<K, V> kv)
+	AVLTreeNode(const pair<K, V>& kv)
 		:_left(nullptr), _right(nullptr), _parent(nullptr), _kv(kv), _bf(0)
 	{}
 };
@@ -61,7 +61,132 @@ public:
 			parent->_left = cur;
 			cur->_parent = parent;
 		}
+		//进行平衡处理
+		while (parent)
+		{
+			if (cur == parent->_right)
+			{
+				parent->_bf += 1;
+			}
+			else
+			{
+				parent->_bf -= 1;
+			}
+			if (parent->_bf == 1 || parent->_bf == -1)
+			{
+				cur = cur->_parent;
+				parent = parent->_parent;
+			}
+			else if (parent->_bf == 2 || parent->_bf == -2)
+			{
+				//旋转处理
+				if (parent->_bf == 2 && cur->_bf == 1)
+				{
+					RotateL(parent);
+				}
+				else if (parent->_bf == -2 && cur->_bf == -1)
+				{
+					RotateR(parent);
+				}
+				break;
+			}
+			else
+			{
+				break;
+			}
+		}
 		return pair<Node*, bool>(cur, true);
+	}
+	void RotateL(Node* parent)
+	{
+		//左单旋
+		Node* subR = parent->_right;
+		Node* subRL = subR->_left;
+
+		parent->_right = subRL;
+		if (subRL)
+			subRL->_parent = parent;
+		subR->_left = parent;
+		Node* parent_par = parent->_parent;
+		
+		if (parent_par == nullptr)
+		{
+			subR = _root;
+			subR->_parent = nullptr;
+		}
+		else
+		{
+			subR->_parent = parent_par;
+			if (parent_par->_right == parent)
+			{
+				parent_par->_right = subR;
+			}
+			else
+			{
+				parent_par->_left = subR;
+			}
+		}
+		parent->_parent = subR;
+		parent->_bf = 0;
+		subR->_bf = 0;
+	}
+	void RotateR(Node* parent)
+	{
+		//右单旋
+		Node* subL = parent->_left;
+		Node* subLR = subL->_right;
+
+		parent->_left = subLR;
+		if (subLR)
+			subLR->_parent = parent;
+		subL->_right = parent;
+		Node* parent_par = parent->_parent;
+		if (parent_par == nullptr)
+		{
+			subL = root;
+			subL->_parent = nullptr;
+		}
+		else
+		{
+			subL->_parent = parent_par;
+			if (parent_par->_right == parent)
+			{
+				parent_par->_right = subL;
+			}
+			else
+			{
+				parent_par->_left = subL;
+			}
+		}
+		parent->_parent = subL;
+		parent->_bf = 0;
+		subL->_bf = 0;
+	}
+	void RotateRL(Node* parent)
+	{
+		Node* subR = parent->_right;
+		Node* subRL = subR->_left;
+		int bf = subRL->_bf;
+		RotateR(parent->_right);
+		RotateL(parent);
+		if (bf == 1)
+		{
+			subRL->_bf = 0;
+			parent->_bf = -1;
+			subR->_bf = 0;
+		}
+		else if (bf == -1)
+		{
+			subRL->_bf = 0;
+			parent->_bf = 0;
+			subR->_bf = 1;
+		}
+		else if (bf == 0)
+		{
+			subRL->_bf = 0;
+			parent = 0;
+			subR->_bf = 0;
+		}
 	}
 private:
 	Node* _root = nullptr;
