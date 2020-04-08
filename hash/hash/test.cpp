dde7b40890714f6567347f16216f2ce3d45f4314
LEIHhang <48359244+LEIHhang@users.noreply.github.com>
2020-04-08 20:14:11 +0800

    updata

--- hash/hash/test.cpp
+++ hash/hash/test.cpp
@@ -1,5 +1,14 @@
-#include"hash.h"
+//#include"hash.h"
+//#include<unordered_map>
+#include<unordered_set>
+using namespace std;
 int main()
 {
-	HASH_BUCKET::test();
+	////HASH_BUCKET::test();
+	//unordered_set<int> m1;
+	//m1.insert(1);
+	//auto pm1 = m1.begin();
+	//cout << *pm1 << endl;
+
+	
 }
